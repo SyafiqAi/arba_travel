e3ab6c1d98af5c9e65c3d23b78873286fa8e5dd4
Syafiq Aiman <isyafiq@gmail.com>
2024-10-20 14:54:26 +0800

    display all posts

--- arba_travel/urls.py
+++ arba_travel/urls.py
@@ -16,10 +16,16 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings  
+from django.urls import path, include  
+from django.conf.urls.static import static  
 
 urlpatterns = [
     path("", include("insta.urls")),
     path('admin/', admin.site.urls),
     path("accounts/", include("accounts.urls")),
-    path("accounts/", include("django.contrib.auth.urls")),  # new
+    path("accounts/", include("django.contrib.auth.urls")), 
 ]
+
+if settings.DEBUG:  # new
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- insta/views.py
+++ insta/views.py
@@ -1,8 +1,11 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 
+from insta.models import Post
+
 def index(request):
-    return render(request, "insta/index.html")
+    context = {"posts": Post.objects.all()}
+    return render(request, "insta/index.html", context)
 
 def create_post(request):
     return render(request, "insta/create_post.html")
