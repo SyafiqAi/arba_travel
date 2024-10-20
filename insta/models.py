c807bc6b555a0ef6363489e8a18cab65f5030b97
Syafiq Aiman <isyafiq@gmail.com>
2024-10-20 14:13:32 +0800

    Post model

--- insta/models.py
+++ insta/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Post(models.Model):
+    title = models.TextField()
+    cover = models.ImageField(upload_to="images/")
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.title
