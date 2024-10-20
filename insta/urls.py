11d0de3b522e563547e93c1a80e4496bfc38bfd0
Syafiq Aiman <isyafiq@gmail.com>
2024-10-20 15:20:14 +0800

    user upload image

--- insta/urls.py
+++ insta/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
+from .views import CreatePostView
 
 from . import views
 
 urlpatterns = [
     path("", views.index, name="index"),
-    path("create_post/", views.create_post, name="create_post")
+    path("create_post/", CreatePostView.as_view(), name="create_post")
 ]
--- insta/views.py
+++ insta/views.py
@@ -1,7 +1,10 @@
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
-
+from django.views.generic import CreateView
 from insta.models import Post
+from django.urls import reverse_lazy
+from .forms import PostForm
+from django.contrib.auth.models import User
 
 def index(request):
     context = {"posts": Post.objects.all()}
@@ -9,3 +12,14 @@ def index(request):
 
 def create_post(request):
     return render(request, "insta/create_post.html")
+
+class CreatePostView(CreateView):
+    model = Post
+    form_class = PostForm
+    template_name = "insta/create_post.html"
+    success_url = reverse_lazy("index")
+    def form_valid(self, form):
+        post = form.save(commit=False)
+        post.user_id = self.request.user.id
+        post.save()
+        return HttpResponseRedirect("/")
