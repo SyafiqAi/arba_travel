6bb2c3b37e7382530ab871efed77dc145c70df77
Syafiq Aiman <isyafiq@gmail.com>
2024-10-20 13:57:24 +0800

    init new post page

--- insta/urls.py
+++ insta/urls.py
@@ -4,4 +4,5 @@ from . import views
 
 urlpatterns = [
     path("", views.index, name="index"),
+    path("create_post/", views.create_post, name="create_post")
 ]
--- insta/views.py
+++ insta/views.py
@@ -3,3 +3,6 @@ from django.shortcuts import render
 
 def index(request):
     return render(request, "insta/index.html")
+
+def create_post(request):
+    return render(request, "insta/create_post.html")
